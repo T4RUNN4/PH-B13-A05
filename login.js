5ea53a327411380d9093072eb353bf20102d1fd1
Tarunna <tarunnasen@gmail.com>
2026-03-06 19:53:05 +0600

    Login Functionality implemented

---
+++ login.js
@@ -0,0 +1,10 @@
+const handleLogin = () => {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "admin123") {
+    window.location.href = "home.html";
+  } else {
+    alert("Invalid credential");
+  }
+};
